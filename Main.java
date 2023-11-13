db5ddb576fcc6854ff9c95cc4afee145d0eeecb5
Aysha-shamli <ayshashamli1999@gmail.com>
2023-11-13 15:37:34 +0530

    added linecalculation.java program

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Display.welcomeMessage);
+
+        lineCalculation myLine = new lineCalculation(3, 4, 7, 1);
+
+        double length = myLine.calculateLength();
+        System.out.println("Length of the line: " + length);
     }
 }
